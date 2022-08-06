dcfc5588c8bc1595372d31f0fc2ecab316c10497
nazmul <nazmulhasan02901@gmail.com>
2022-08-06 22:37:24 +0600

    up to post request

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import productRouters from "./routers/productRouters.js";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 const app = express();
+app.use(express.json());
 dotenv.config();
 
-app.route("/").get((req, res,nex) => {res.send("this is a sample api request")})
+app.route("/").get((req, res, nex) => {
+  res.send("this is a sample api request");
+});
 
 //================ MOngodb connection is here ===================
 mongoose
--- Backend/routers/productRouters.js
+++ Backend/routers/productRouters.js
@@ -2,11 +2,17 @@ import express from "express";
 import Product from "../models/productSchema.js";
 const router = express.Router();
 
-router.post("/product/create", async (req, res, nex) => {
-  const created_product = await Product.create(req.body);
-  res.status(201).json({
-    success: true,
-    created_product,
-  });
+router.route("/product/create").post(async (req, res, nex) => {
+  try {
+    console.log(req.body);
+    const created_product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      created_product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 export default router;
