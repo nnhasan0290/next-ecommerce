54052c1065954f31cb52be6e7fab10059fd63ab2
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-08-09 15:17:39 +0000

    form data

--- Frontend/components/dashboard/DashContent.js
+++ Frontend/components/dashboard/DashContent.js
@@ -7,12 +7,17 @@ import { useState } from "react";
 
 const DashContent = () => {
   const [inputImg, setInputImg] = useState([]);
+  const [name, setName] = useState("");
+  const [stock, setStock] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [desc, setDesc] = useState("");
+  const [cat, setCat] = useState("");
 
   //Methods ===========================
   const fileUpload = (e) => {
     const files = Array.from(e.target.files);
     setInputImg([]);
-    
+
     files.forEach((each) => {
       const reader = new FileReader();
 
@@ -24,30 +29,56 @@ const DashContent = () => {
       reader.readAsDataURL(each);
     });
   };
+
+  //submit method here ====================
+  const submitAndcreateProduct = (e) => {
+    e.preventDefault();
+    const myForm = new FormData();
+    form.set("name", name);
+    form.set("price", price);
+    form.set("cat", cat);
+    form.set("desc", desc);
+    form.set("stock", stock);
+    inputImg.forEach((each) => {
+      form.append("images", each);
+    });
+    console.log(myForm);
+  };
   return (
     <div className="sm:w-[80%] flex justify-center items-center sm:float-right bg-[#f9f9f9] ">
       <div className="px-10 py-3 border shadow-3xl">
         <h2 className="text-center big-heading">Create Product</h2>
-        <form action="" className="m-3">
+        <form onSubmit={submitAndcreateProduct} action="" className="m-3">
           <div className="input-parent">
             <CheckCircleIcon className="h-7" />
             <input
+              onChange={(e) => setName(e.target.value)}
               className="create-input"
               type="text"
+              value={name}
               placeholder="product name"
               required
             />
           </div>
           <div className="input-parent">
             <CurrencyDollarIcon className="h-7" />
-            <input type="number" placeholder="price" min={20} required />
+            <input
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              type="number"
+              placeholder="price"
+              min={20}
+              required
+            />
           </div>
           <div className="items-start input-parent">
             <DocumentTextIcon className="h-7" />
             <textarea
+              onChange={(e) => setDesc(e.target.value)}
               className="w-full outline-none resize-none bg-whit leading-widest placeholder:capitalize"
               name="description"
               placeholder="product description"
+              value={desc}
               wrap="physical"
               id=""
               cols="10"
@@ -56,19 +87,25 @@ const DashContent = () => {
           </div>
           <div className="input-parent">
             <p> category</p>
-            <select className="capitalize outline-none">
+            <select
+              onChange={(e) => setCat(e.target.value)}
+              className="capitalize outline-none"
+            >
+              <option value="Select">Select</option>
               <option value="laptop">Laptop</option>
               <option value="desktop">desktop</option>
-              <option value="three">Loption three</option>
+              <option value="three">Electronics</option>
               <option value="four">four</option>
             </select>
           </div>
           <div className="input-parent">
             <input
+              onChange={(e) => setStock(e.target.value)}
               type="number"
               name="stock"
               id="stock"
               placeholder="Stock"
+              value={stock}
               min={1}
               max={50}
               required
@@ -86,11 +123,9 @@ const DashContent = () => {
             />
           </div>
           <div className="flex flex-wrap items-center max-w-[300px]">
-            {
-              inputImg.map((each,i) => (
-                  <img key={i} src={each} alt="product-img" width={100}  />
-              ))
-            }
+            {inputImg.map((each, i) => (
+              <img key={i} src={each} alt="product-img" width={100} />
+            ))}
           </div>
           <input
             name="product"
